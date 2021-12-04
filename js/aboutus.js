c6153ab0ea76359f24025f908f119a38628304ad
A Hunter <amanda.hunter@flemingcollege.ca>
2021-12-04 10:29:55 -0500

    working on a button but in the building blocks

---
+++ js/aboutus.js
@@ -0,0 +1,6 @@
+//  Use JavaScript to Toggle Hide/Show <p> elements on click for blag articles  SOURCE:https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show 
+
+function myFunction(){
+    document.getElementById("blagC").ontoggle = function() {myFunction()};
+
+    
