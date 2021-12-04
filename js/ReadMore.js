040cc4c709b4ea78ad8d373dd35d7868fca7413f
Gabby Riefesel <gabby.riefesel@flemingcollege.ca>
2021-12-03 23:44:01 -0330

    blag pag revamped SO MUCH

--- js/ReadMore.js
+++ js/ReadMore.js
@@ -2,11 +2,7 @@
 //  Use JavaScript to Toggle Hide/Show <p> element on click for blag articles  SOURCE:https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show 
 
     function myFunction(){
-        var x = document.getElementsById("blagJ");
-        if(x.style.display === "none") {
-            x.style.display = "block";
-        } else {
-            x.style.display = "none";
-        }
-    }
+        document.getElementById("blagC").ontoggle = function() {myFunction()};
+
+
   
