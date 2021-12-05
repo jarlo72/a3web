e462f42b4c51867578833915009f2653af190bc9
A Hunter <amanda.hunter@flemingcollege.ca>
2021-12-05 06:42:05 -0500

    js fixing

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,21 +1,23 @@
 //  Use JavaScript to Toggle Hide/Show <p> elements on click for blag articles  SOURCE:https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show 
 
 /*function squirrelsummon(){
-    document.getElementById("squirrel").onmouseover.src='squirrels121.jpg' =function() {squirrelsummon()};*/
+    document.getElementById("squirrel").onmouseover="this.src='squirrels121.jpg'"onmouseout="this.src = 'squirrel121bw'"" =function() {squirrelsummon()};*/
     
-    
-    
- /*   function changeDef(event){ 
-        console.log(event.target);
-      }*/
-    /*"onmouseout="this.src='squirrel121bw'"*/
-    
-   /* onhover */
+  function squirrelsummon(){document.getElementById("squirrel").src="images\profiles\squirrels121.jpg";
 
-   var img = document.querySelector("img.squirrel");
+  }  
+  function oldquirrel(){document.getElementById("squirrel").src="images\profiles\squirrel121bw.jpg";
+
+  }  
+  /* https://www.youtube.com/watch?v=AiiXLmqhndU*/
+
+   /*var img = document.querySelector("img.squirrel");
    img.addEventListener("mouseover", squirrelsummon);
    function dquirrelsummon(event){
     console.log(event.target);
-  }
+  }*/
+
+
+  /* https://stackoverflow.com/questions/49350534/how-to-call-js-function-on-hover-from-html */
 
 
