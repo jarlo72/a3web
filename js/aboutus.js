94eaa3fe233768201539d8045a7e4f0791137d52
A Hunter <amanda.hunter@flemingcollege.ca>
2021-12-05 07:22:35 -0500

    link the js

--- js/aboutus.js
+++ js/aboutus.js
@@ -3,12 +3,11 @@
 /*function squirrelsummon(){
     document.getElementById("squirrel").onmouseover="this.src='squirrels121.jpg'"onmouseout="this.src = 'squirrel121bw'"" =function() {squirrelsummon()};*/
     
-  function squirrelsummon(){document.getElementById("squirrel").src="images\profiles\squirrels121.jpg";
+  function squirrelsummon(){document.getElementById("squirrel").src="images\profiles\squirrels121.jpg";}  
 
-  }  
-  function oldquirrel(){document.getElementById("squirrel").src="images\profiles\squirrel121bw.jpg";
+  function oldquirrel(){document.getElementById("squirrel").src="images\profiles\squirrel121bw.jpg";}  
 
-  }  
+
   /* https://www.youtube.com/watch?v=AiiXLmqhndU*/
 
    /*var img = document.querySelector("img.squirrel");
