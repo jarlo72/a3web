9b91d49eaba9dccb575b51c965bf427f39ab77e5
A Hunter <amanda.hunter@flemingcollege.ca>
2021-12-04 23:31:12 -0500

    javasquirrel

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,7 +1,7 @@
 //  Use JavaScript to Toggle Hide/Show <p> elements on click for blag articles  SOURCE:https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show 
 
 function squirrelsummon(){
-    document.getElementById("squirrel").onmouseover="this.src='squirrels121.jpg'" =function() {squirrelsummon()};
+    document.getElementById("squirrel").onmouseover.src='squirrels121.jpg' =function() {squirrelsummon()};
     
     
     
