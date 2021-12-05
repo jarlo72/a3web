eba8da441510bfd605bcb344c6060176e3206325
A Hunter <amanda.hunter@flemingcollege.ca>
2021-12-04 23:28:47 -0500

    java

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,9 +1,13 @@
 //  Use JavaScript to Toggle Hide/Show <p> elements on click for blag articles  SOURCE:https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show 
 
 function squirrelsummon(){
-    document.getElementById("squirrel").onmouseover="this.src='squirrels121.jpg'"= function() {squirrelsummon()};
+    document.getElementById("squirrel").onmouseover="this.src='squirrels121.jpg'" =function() {squirrelsummon()};
     
     
+    
+ /*   function changeDef(event){ 
+        console.log(event.target);
+      }*/
     /*"onmouseout="this.src='squirrel121bw'"*/
     
    /* onhover */
